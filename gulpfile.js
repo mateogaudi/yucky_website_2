691cc287f3bfe5ad68356e09cc64d5178f7f4cbf
Kamil Bartusik <kmb-it@localhost.localdomain>
2021-04-25 12:47:35 +0200

    project structure fix, implement gulp

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let babel = require('gulp-babel');
+let uglify = require('gulp-uglify-es').default;
+let concat = require("gulp-concat");
+let cleanCSS = require("gulp-clean-css");
+let autoprefixer = require("gulp-autoprefixer");
+let eslint = require("gulp-eslint");
+
+sass.compiler = require("node-sass");
+
+gulp.task("sass", function() {
+    return gulp
+        .src([
+            "./dev/scss/_boot.scss",
+        ])
+        .pipe(concat("all.min.css"))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(
+            autoprefixer({
+                cascade: false
+            })
+        )
+        .pipe(
+            cleanCSS({
+                compatibility: "ie8"
+            })
+        )
+        .pipe(gulp.dest("./dist/style"));
+});
+
+gulp.task("debugJs", function() {
+    return gulp
+        .src("./dev/js/**/*.js")
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task("compressJs", function() {
+    return gulp
+        .src([
+            "./node_modules/jquery/dist/jquery.js",
+            "./dev/js/*.js"
+        ])
+        .pipe(concat("all.min.js"))
+        .pipe(uglify())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch("./dev/scss/**/*.scss", gulp.series("sass"));
+    gulp.watch("./dev/js/**/*.js", gulp.series("compressJs"));
+});
